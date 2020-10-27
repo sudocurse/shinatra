8d7c9c9e24d4ce9df93877e682c0f084eca17e2e
Ankeet Presswala <sudocurse@gmail.com>
2020-10-27 01:33:41 -0700

    makes headers more readable; adds content length and connection close

--- shinatra-json.sh
+++ shinatra-json.sh
@@ -12,11 +12,13 @@ list(){
         sed 's/^/[/;s/.$/]/'                              | \
         jq)
 }
-
 make_response() {
     body=$(list)
-    echo -en "HTTP/1.1 200 OK\r\nConnection: keep-alive\r\nContent-Type: application/json\r\nContent-Length: $(echo $body | wc -c)\r\n\r\n
-$body\r\n\r\n"
+    echo -en "HTTP/1.1 200 OK\r\n"\
+"Connection: close\r\n"\
+"Content-Type: application/json\r\n"\
+"Content-Length: $(echo $body | wc -c)\r\n\r\n"\
+"$body\r\n\r\n\r\n\r\n"
 }
 echo -en "$(make_response)"
 while { echo -en "$(make_response)"; } | nc -l "${1:-8080}"; do
